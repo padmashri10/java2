1c0d8c4cc90b42c7d200b1e34429c4a724f9cd82
padmashri10 <51259189+padmashri10@users.noreply.github.com>
2019-06-06 20:55:32 +0530

    example on static inner class

---
+++ inner.java
@@ -0,0 +1,22 @@
+ public class Outer 			
+    int x = 10;
+    static int y = 20;
+    static class Inner 		
+    {
+        void show()		        
+        {
+            System.out.println(y);
+        }
+    }
+    public static  void main(String... s)
+    {
+        System.out.println("'In main method'. The value of static data member of outer class is :"+y);
+      
+        System.out.println("Inner class method accessed by Inner class object ");
+        Inner i = new Inner();
+        i.show();
+        System.out.println("Inner class method accessed by outer class object ");
+        Outer.Inner o = new Outer.Inner();
+        o.show();
+    }
+}
